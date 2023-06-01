93cd3d2e4e10145d6bce65cd98676be3f380c2de
Mohannad Adnan Alassar <mohannad.alassar@hotmail.com>
2023-06-01 07:48:45 -0400

    ENV

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 const port = 4000; // Server Port
 // Make the Server Listening in Event Loop
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`App Running on Port ${process.env.PORT}`);
 });
