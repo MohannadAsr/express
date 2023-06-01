daf52803f551b11a9cb9d1edef932a00d87abf29
Mohannad Adnan Alassar <mohannad.alassar@hotmail.com>
2023-06-01 07:44:17 -0400

    vercel json

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 const port = 4000; // Server Port
 // Make the Server Listening in Event Loop
-app.listen(process.env.PORT, () => {
+app.listen(4000, () => {
   console.log(`App Running on Port ${process.env.PORT}`);
 });
